3b56b9f4b59cb05657d7317b68293c3a8015c788
fernandoporazzi <fernandoporazzi@gmail.com>
2017-03-30 03:50:34 -0300

    Create upload and insert new movie

--- movie/controller.go
+++ movie/controller.go
@@ -2,9 +2,19 @@ package movie
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"time"
+
+	"crypto/md5"
+	"encoding/hex"
+
+	"strconv"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var (
@@ -18,6 +28,59 @@ func init() {
 	}
 
 	database = session.DB("techparty-golang")
+}
+
+func getHash() string {
+	crutime := time.Now().Unix()
+	h := md5.New()
+	io.WriteString(h, strconv.FormatInt(crutime, 10))
+	hashInBytes := h.Sum(nil)[:16]
+	return hex.EncodeToString(hashInBytes)
+}
+
+func uploadImage(h string, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+
+	file, _, err := r.FormFile("cover")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile("./uploads/"+h+".jpg", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, file); err != nil {
+		panic(err)
+	}
+}
+
+func insertMovie(h string, r *http.Request) Movie {
+	c := database.C("movie")
+
+	var m Movie
+
+	y, err := strconv.Atoi(r.FormValue("year"))
+	if err != nil {
+		panic(err)
+	}
+
+	m.ID = bson.NewObjectId()
+	m.Name = r.FormValue("name")
+	m.Year = y
+	m.Synopsis = r.FormValue("synopsis")
+	m.Cover = h + ".jpg"
+	m.Categories = r.Form["category"]
+
+	err = c.Insert(m)
+	if err != nil {
+		panic(err)
+	}
+
+	return m
 }
 
 // GetAllMovies retrieves all movies in the database
@@ -41,16 +104,16 @@ func GetAllMovies(w http.ResponseWriter, r *http.Request) {
 
 // CreateMovie exported
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
-	// c := database.C("movie")
+	h := getHash()
 
-	var m Movie
+	uploadImage(h, r)
 
-	err := json.NewDecoder(r.Body).Decode(&m)
-
-	if err != nil {
-		panic(err)
-	}
+	n := insertMovie(h, r)
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(n); err != nil {
+		panic(err)
+	}
 }
--- movie/model.go
+++ movie/model.go
@@ -6,10 +6,12 @@ import (
 
 // Movie is exported in order to create or update database documents
 type Movie struct {
-	ID    bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Name  string        `json:"name"`
-	Year  int           `json:"year"`
-	Cover string        `json:"cover"`
+	ID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Name       string        `json:"name"`
+	Year       int           `json:"year"`
+	Synopsis   string        `json:"synopsis"`
+	Cover      string        `json:"cover"`
+	Categories []string      `json:"categories"`
 }
 
 // Movies exported as collection of Movie
